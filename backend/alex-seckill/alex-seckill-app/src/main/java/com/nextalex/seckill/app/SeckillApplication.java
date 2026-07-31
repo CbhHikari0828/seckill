d7d7afbeb368d9ecb9937f999f935dcfd568733e
alex <alexlee0828cbh@gmail.com>
2026-07-31 10:54:22 +0800

    项目初始化框架各项服务搭建完毕

---
+++ backend/alex-seckill/alex-seckill-app/src/main/java/com/nextalex/seckill/app/SeckillApplication.java
@@ -0,0 +1,20 @@
+package com.nextalex.seckill.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Author: nextalex
+ * @Date: 2026/7/31 10:34
+ * @Version: v1.0.0
+ * @Description: 秒杀系统启动类
+ **/
+@SpringBootApplication
+@ComponentScan({"com.nextalex.seckill.*"}) // 多模块项目中，必需手动指定扫描 com.quanxiaoha.seckill 包下面的所有类
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+}
