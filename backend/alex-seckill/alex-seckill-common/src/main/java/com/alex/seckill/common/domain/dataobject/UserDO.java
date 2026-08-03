7292854e8299158fba022330870262ebb2ea77ef
alex <alexlee0828cbh@gmail.com>
2026-08-03 17:08:47 +0800

    完成了 MyBatis 和 HikariCP 的整合

---
+++ backend/alex-seckill/alex-seckill-common/src/main/java/com/alex/seckill/common/domain/dataobject/UserDO.java
@@ -0,0 +1,34 @@
+package com.alex.seckill.common.domain.dataobject;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserDO {
+    private Long id;
+
+    private String nickname;
+
+    private String password;
+
+    private String mobile;
+
+    private String avatar;
+
+    private Integer status;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+
+}
